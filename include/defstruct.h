47eba304485983a2eb7237d7acfbb1a726241554
holyogre <2510907980@qq.com>
2025-12-05 19:51:26 +0800

    测试

--- include/defstruct.h
+++ include/defstruct.h
@@ -74,7 +74,7 @@ namespace track_project
     };
 
     /*****************************************************************************
-     * @brief 流水线架构：TrackingBuffer，计划为每个服务设计一个环形缓冲区结构体
+     * @brief 流水线架构：TrackingBuffer，计划设计为环形缓冲区结构体
      * 特性：
      * 1. 下级流水线只准用到上级流水线(和 **预处理线程** )的数据
      * 2. 表示航迹，点迹大小情况统一使用std::uint32_t确保数据长度一致性和非负性
@@ -109,18 +109,142 @@ namespace track_project
         };
 
         /*****************************************************************************
-         * @brief 点航关联结构&卡尔曼滤波，第一段流水线：
+         * @brief 航迹关联流水线结构，第一段流水线：
          * 输入条件：环形缓冲区存在空闲段（此时航迹管理刚刚处理完）
          * 输出条件：无
          * 至少占用一个线程，是否占用GPU待定
          *****************************************************************************/
-        // 剩余点迹 
+        // 关联点迹结构
         struct AssociatedPoint
         {
-            bool is_associated; // 关联情况
+            std::uint32_t track_id;
+            std::uint32_t point_id;
+
+            // 卡尔曼滤波预处理，不考虑曲率
+            double vx;
+            double vy;
         };
 
+        /*****************************************************************************
+         * @brief 航迹起批流水线结构&卡尔曼滤波流水线结构，组成并行的第二段流水线：
+         * 输入条件：环形缓冲区存在空闲段（此时航迹关联刚刚处理完）
+         * 输出条件：无
+         * 至少占用三个线程,起批需使用GPU
+         *****************************************************************************/
         // 航迹结构
+        struct NewTrack
+        {
+            bool is_ais;
+            std::uint32_t point_num;
+
+            // 结构待定，给个定长缓冲区吧，反正也没多少内存
+        };
+
+        // 卡尔曼滤波点迹
+        struct PredictedPoint
+        {
+            std::uint32_t track_id;
+            std::uint32_t point_id;
+
+            bool is_updated; // 表示该点是否被更新
+
+            // 卡尔曼滤波器结果
+            double x;
+            double y;
+            double vx;
+            double vy;
+        };
+
+        /*****************************************************************************
+         * @brief 航迹管理流水线结构，组成第三段流水线：
+         * 输入条件：环形缓冲区存在空闲段（此时航迹管理&卡尔曼滤波刚刚处理完）
+         * 输出条件：UDP发送成功
+         * 至少占用一个线程，不占用GPU
+         *****************************************************************************/
+        struct ExistTrack
+        {
+            std::uint32_t track_id = 0;
+            std::uint32_t extrapolation_count = 0;
+            std::uint32_t point_num = 0;
+            int state = 3; // 0表示航迹正常，1表示航迹外推，2表示航迹终结
+
+            double longitude;
+            double latitude;
+            double sog;      // 对地速度,m/s
+            double cog;      // 对地航向,北偏东角度，度
+            double angle;    // 雷达观测角度，雷达法线顺时针方向，度
+            double distance; // 雷达观测距离，距离雷达站距离，km
+
+            // 卡尔曼滤波预处理，不考虑曲率，辅助关联点迹计算下vx,vy
+            double x;
+            double y;
+        };
+
+        /*****************************************************************************
+         * @brief 缓冲区定义，TODO，我想把VECTOR的扩容给删了，改成一开始就开够空间
+         *****************************************************************************/
+        struct TrackingBuffer
+        {
+            // 检测点迹结构
+            std::vector<DetectedPoint> detected_point;
+
+            // 关联点迹申明
+            std::vector<AssociatedPoint> associated_point;
+
+            // 起批结构
+            std::vector<NewTrack> new_track;
+
+            // 确定部分
+            std::vector<PredictedPoint> predicted_point; // 预测结构
+            std::vector<ExistTrack> existed_point;
+        };
+
+    } // namespace pipeline
+
+    /*****************************************************************************
+     * @brief 通信结构，内存有序排列，方便定长数据读取
+     *****************************************************************************/
+    namespace communicate
+    {
+// 航迹头，请确保数据是完全自包含的数据块，固定4位存一个数字
+#pragma pack(push, 4)
+        struct TrackerHeader
+        {
+            std::uint32_t track_id = 0;
+            std::uint32_t extrapolation_count = 0;
+            std::uint32_t point_num = 0;
+            int state = 3; // 0表示航迹正常，1表示航迹外推，2表示航迹终结
+
+            void start(std::uint32_t id)
+            {
+                track_id = id;
+                extrapolation_count = 0;
+                point_num = 0;
+                state = 0;
+            };
+
+            void clear()
+            {
+                track_id = 0;
+                extrapolation_count = 0;
+                point_num = 0;
+                state = -1;
+            };
+
+            friend std::ostream &operator<<(std::ostream &os, const TrackerHeader &header)
+            {
+                os << "TrackerHeader [";
+                os << "track_id=" << header.track_id << ", ";
+                os << "extrapolation_count=" << header.extrapolation_count;
+                os << "]";
+
+                return os;
+            }
+        };
+#pragma pack(pop)
+
+// 航迹点标准结构，固定八字节存储
+#pragma pack(push, 8)
         struct TrackPoint
         {
             double longitude;
@@ -146,49 +270,13 @@ namespace track_project
                 return os;
             }
         };
+#pragma pack(pop)
 
-        /*****************************************************************************
-         * @brief 航迹起批流水线结构&卡尔曼滤波流水线结构，组成并行的第二段流水线：
-         * 输入条件：环形缓冲区存在空闲段（此时航迹关联刚刚处理完）
-         * 输出条件：无
-         * 至少占用三个线程,起批需使用GPU
-         *****************************************************************************/
-        // 航迹点标准结构，固定八字节存储
-
-
-
-    } // namespace pipeline
-
-    /*****************************************************************************
-     * @brief 通信结构，内存有序排列，方便定长数据读取
-     *****************************************************************************/
-    namespace communicate
-    {
-        // 航迹头，请确保数据是完全自包含的数据块，固定4位存一个数字
-        struct TrackerHeader
-        {
-            std::uint32_t track_id = 0;
-            std::uint32_t extrapolation_count = 0;
-            std::uint32_t point_num = 0;
-            int state = 3; // 0表示航迹正常，1表示航迹外推，2表示航迹终结
-
-            void start(std::uint32_t id)
-            {
-                track_id = id;
-                extrapolation_count = 0;
-                point_num = 0;
-                state = 0;
-            };
-
-            void clear()
-            {
-                track_id = 0;
-                extrapolation_count = 0;
-                point_num = 0;
-                state = -1;
-            };
-        };
-
+        // 约束
+        static_assert(std::is_trivially_copyable_v<TrackPoint>, "TrackPoint 不是平凡的");
+        static_assert(std::is_standard_layout_v<TrackPoint>, "TrackPoint 不是内存连续的");
+        static_assert(std::is_trivially_copyable_v<TrackerHeader>, "TrackerHeader 不是平凡的");
+        static_assert(std::is_standard_layout_v<TrackerHeader>, "TrackerHeader 不是内存连续的");
     } // namespace communicate
 
 } // track_project
